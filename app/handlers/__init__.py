7d2cef543a6b3b6807a113e963fdf08d808a5866
tarassirak902-creator <tarassirak902@gmail.com>
2026-08-05 00:50:50 +0700

    Register payment entry UI before legacy profile callbacks

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -17,6 +17,8 @@ from . import admin_commands
 from . import casper_game
 from . import menus
 from . import profile_achievements
+# Payment/profile entry adapters must be registered before legacy profile callbacks.
+from . import payment_entry_ui
 from . import callbacks_profile
 from . import referrals
 from . import callbacks_duels
