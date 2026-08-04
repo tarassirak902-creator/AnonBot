eec57f694ac6c003ba229bf7fe8872f5a5ad943a
tarassirak902-creator <tarassirak902@gmail.com>
2026-08-04 23:12:08 +0700

    Export question presentation helpers

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,11 +4,23 @@ Services coordinate temporary state and domain workflows without depending on
 Telegram handlers or direct database details.
 """
 
+from .question_presentation import (
+    QuestionListItem,
+    build_answer_list_items,
+    build_question_list_items,
+    display_owner_name,
+    format_question_timestamp,
+)
 from .question_receiver import QuestionReceiverResolver
 from .question_start_context import QuestionStartContext, QuestionStartContextStore
 
 __all__ = [
+    "QuestionListItem",
     "QuestionReceiverResolver",
     "QuestionStartContext",
     "QuestionStartContextStore",
+    "build_answer_list_items",
+    "build_question_list_items",
+    "display_owner_name",
+    "format_question_timestamp",
 ]
