75f665e349f773ea427c87ff21074105b800e656
tarassirak902-creator <tarassirak902@gmail.com>
2026-08-06 18:23:07 +0700

    Feature: register Platform 3.0 social center

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -40,6 +40,7 @@ from . import commercial_profile_aliases
 from . import commercial_navigation_ui
 from . import commercial_daily_hub
 from . import platform_commercial_ui
+from . import platform_social_ui
 from . import navigation_fallback_ui
 # Unified admin and user entry screens must be registered before legacy handlers.
 from . import admin_overview_ui
