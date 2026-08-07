be51c8233b6f865215a40bfecac2387219b77f77
tarassirak902-creator <tarassirak902@gmail.com>
2026-08-07 16:15:11 +0700

    Navigation: route weekly event back to rewards

--- app/handlers/commercial_profile_aliases.py
+++ app/handlers/commercial_profile_aliases.py
@@ -7,4 +7,4 @@ from .shared import router
 
 @router.callback_query(F.data == "weekly_event")
 async def weekly_event_commercial_alias(callback: CallbackQuery) -> None:
-    await _render_event(callback)
+    await _render_event(callback, parent="rewards")
